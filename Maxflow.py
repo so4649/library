75ee3826294ee4ba02771a72d508d0099305160c
so4649 <70653894+so4649@users.noreply.github.com>
2020-11-14 02:41:29 +0900

    Add files via upload

---
+++ Maxflow.py
@@ -0,0 +1,126 @@
+# Dinic's algorithm
+# O(|V|2|E|)
+from collections import deque
+class Dinic:
+    def __init__(self, N):
+        self.N = N
+        self.G = [[] for i in range(N)]
+
+    # 1方向
+    def add_edge(self, fr, to, cap):
+        forward = [to, cap, None]
+        forward[2] = backward = [fr, 0, forward]
+        self.G[fr].append(forward)
+        self.G[to].append(backward)
+
+    # 両方向
+    def add_multi_edge(self, v1, v2, cap1, cap2):
+        edge1 = [v2, cap1, None]
+        edge1[2] = edge2 = [v1, cap2, edge1]
+        self.G[v1].append(edge1)
+        self.G[v2].append(edge2)
+
+    def bfs(self, s, t):
+        self.level = level = [None]*self.N
+        deq = deque([s])
+        level[s] = 0
+        G = self.G
+        while deq:
+            v = deq.popleft()
+            lv = level[v] + 1
+            for w, cap, _ in G[v]:
+                if cap and level[w] is None:
+                    level[w] = lv
+                    deq.append(w)
+        return level[t] is not None
+
+    def dfs(self, v, t, f):
+        if v == t:
+            return f
+        level = self.level
+        for e in self.it[v]:
+            w, cap, rev = e
+            if cap and level[v] < level[w]:
+                d = self.dfs(w, t, min(f, cap))
+                if d:
+                    e[1] -= d
+                    rev[1] += d
+                    return d
+        return 0
+
+    def flow(self, s, t):
+        flow = 0
+        INF = 10**9 + 7
+        G = self.G
+        while self.bfs(s, t):
+            *self.it, = map(iter, self.G)
+            f = INF
+            while f:
+                f = self.dfs(s, t, INF)
+                flow += f
+        return flow
+
+n,k = map(int,input().split())
+work = [list(map(lambda x:int(x)-1,input().split())) for i in range(n)]
+
+f = Dinic(n+k+2)
+s,t = n+k,n+k+1
+
+for i in range(n):
+    f.add_edge(s,i,1)
+
+for i in range(k):
+    f.add_edge(n+i,t,1)
+
+for i in range(n):
+    for j in work[i]:
+        f.add_edge(i,n+j,1)
+
+ans = f.flow(s,t)
+print(ans)
+
+# Ford-Fulkerson algorithm
+# 流すフローをFとすると O(F|E|)
+class FordFulkerson:
+    def __init__(self, N):
+        self.N = N
+        self.G = [[] for i in range(N)]
+
+    # 1方向
+    def add_edge(self, fr, to, cap):
+        forward = [to, cap, None]
+        forward[2] = backward = [fr, 0, forward]
+        self.G[fr].append(forward)
+        self.G[to].append(backward)
+
+    # 両方向
+    def add_multi_edge(self, v1, v2, cap1, cap2):
+        edge1 = [v2, cap1, None]
+        edge1[2] = edge2 = [v1, cap2, edge1]
+        self.G[v1].append(edge1)
+        self.G[v2].append(edge2)
+
+    def dfs(self, v, t, f):
+        if v == t:
+            return f
+        used = self.used
+        used[v] = 1
+        for e in self.G[v]:
+            w, cap, rev = e
+            if cap and not used[w]:
+                d = self.dfs(w, t, min(f, cap))
+                if d:
+                    e[1] -= d
+                    rev[1] += d
+                    return d
+        return 0
+
+    def flow(self, s, t):
+        flow = 0
+        f = INF = 10**9 + 7
+        N = self.N
+        while f:
+            self.used = [0]*N
+            f = self.dfs(s, t, INF)
+            flow += f
+        return flow
