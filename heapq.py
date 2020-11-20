7e01cb3026951ecea0905a8969707321360ce454
so4649 <70653894+so4649@users.noreply.github.com>
2020-11-20 20:23:49 +0900

    Add files via upload

--- heapq.py
+++ heapq.py
@@ -67,3 +67,52 @@ class Heapq:
 #初期化時 desc=Trueを指定していたなら大きい方が出てきます。
 #pop()に似てますが、参照するだけで、集合から値が消えることはありません。
 #O(1)でめっちゃ速いです
+
+
+# https://socha77.hatenablog.com/entry/2020/06/17/012842
+# 遅延ヒープ
+from collections import defaultdict
+import heapq
+
+class LazyHeap():
+    def __init__(self, init_arr=[]):
+        self.heap = []
+        self.lazy = defaultdict(int)
+        self.len = 0
+        for init_element in init_arr:
+            heapq.heappush(self.heap, init_element)
+            self.len += 1
+ 
+    def __len__(self):
+        return self.len
+ 
+    def push(self, k):
+        heapq.heappush(self.heap, k)
+        self.len += 1
+ 
+    def pop(self):
+        self._clear()
+        return heapq.heappop(self.heap)
+        
+    def top(self):
+        self._clear()
+        return self.heap[0]
+ 
+    def _clear(self):
+        while True:
+            cand = self.heap[0]
+            if cand in self.lazy and self.lazy[cand] > 0:
+                heapq.heappop(self.heap)
+                self.lazy[cand] -= 1
+ 
+            else:
+                return
+ 
+    def remove(self, k):
+        self.lazy[k] += 1
+        self.len -= 1
+
+a = [1, 6, 8, 0, -1]
+q = LazyHeap(a)
+q.remove(-1)
+print(q.top())
