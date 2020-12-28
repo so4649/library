271f34dacaa49cebd91a3ef526214cdd732fb986
so4649 <70653894+so4649@users.noreply.github.com>
2020-12-29 05:51:49 +0900

    Add files via upload

---
+++ matrix.py
@@ -0,0 +1,14 @@
+def mmult(A, B):
+    global mod
+    n, m, l = len(A), len(B), len(B[0])
+    ret = [[0]*l for _ in range(n)]
+    for i in range(n):
+        for j in range(m):
+            for k in range(l):
+                ret[i][k] = (ret[i][k]+A[i][j]*B[j][k])%mod
+    return ret
+    
+def mpow(A, n):
+    if n == 0: return [[1 if i==j else 0 for j in range(len(A))] for i in range(len(A))]
+    if n % 2: return mmult(mpow(A, n-1), A)
+    return mpow(mmult(A, A), n//2)
