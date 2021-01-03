a1d9407a13f14b293b1d697087fa5119948679cd
so4649 <70653894+so4649@users.noreply.github.com>
2021-01-04 01:28:37 +0900

    Add files via upload

--- Minflow.py
+++ Minflow.py
@@ -1,4 +1,5 @@
 # 最小費用流(minimum cost flow)
+# with Bellman-Ford Algorithm
 # O(F|V||E|)
 # 各辺eが容量ceと単位コストdeを持つ、有向グラフG=(V,E)に対し、 
 # 特定のsourceとsinkの間に流量Fを流す時の最小コストを求める。
@@ -50,4 +51,69 @@ class MinCostFlow:
                 e[1] -= d
                 G[v][e[3]][1] += d
                 v = prevv[v]
+        return res
+
+# with Dijkstra’s Algorithm
+# ポテンシャルを導入して、ダイクストラ法を用いて最短路を計算するようにしたもの。
+# O(F|E|log|V|)
+from heapq import heappush, heappop
+class MinCostFlow:
+    INF = 10**18
+
+    def __init__(self, N):
+        self.N = N
+        self.G = [[] for i in range(N)]
+
+    def add_edge(self, fr, to, cap, cost):
+        forward = [to, cap, cost, None]
+        backward = forward[3] = [fr, 0, -cost, forward]
+        self.G[fr].append(forward)
+        self.G[to].append(backward)
+
+    def flow(self, s, t, f):
+        N = self.N; G = self.G
+        INF = MinCostFlow.INF
+
+        res = 0
+        H = [0]*N
+        prv_v = [0]*N
+        prv_e = [None]*N
+
+        d0 = [INF]*N
+        dist = [INF]*N
+
+        while f:
+            dist[:] = d0
+            dist[s] = 0
+            que = [(0, s)]
+
+            while que:
+                c, v = heappop(que)
+                if dist[v] < c:
+                    continue
+                r0 = dist[v] + H[v]
+                for e in G[v]:
+                    w, cap, cost, _ = e
+                    if cap > 0 and r0 + cost - H[w] < dist[w]:
+                        dist[w] = r = r0 + cost - H[w]
+                        prv_v[w] = v; prv_e[w] = e
+                        heappush(que, (r, w))
+            if dist[t] == INF:
+                return None
+
+            for i in range(N):
+                H[i] += dist[i]
+
+            d = f; v = t
+            while v != s:
+                d = min(d, prv_e[v][1])
+                v = prv_v[v]
+            f -= d
+            res += d * H[t]
+            v = t
+            while v != s:
+                e = prv_e[v]
+                e[1] -= d
+                e[3][1] += d
+                v = prv_v[v]
         return res
