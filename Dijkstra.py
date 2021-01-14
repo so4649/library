68e455ccfe4fc3c2e0413326a6ddfd4b9f30ba4a
so4649 <70653894+so4649@users.noreply.github.com>
2021-01-15 02:27:22 +0900

    Add files via upload

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,17 +1,16 @@
 # https://nashidos.hatenablog.com/entry/2020/04/07/100508
 
 import heapq
-
 def dijkstra(s):
     hq = [(0, s)]
-    heapq.heapify(hq) # リストを優先度付きキューに変換
-    cost = [float('inf')] * n # 行ったことのないところはinf
-    cost[s] = 0 # 開始地点は0
+    heapq.heapify(hq)
+    cost = [float('inf')] * n
+    cost[s] = 0
     while hq:
         c, v = heapq.heappop(hq)
-        if c > cost[v]: # コストが現在のコストよりも高ければスルー
+        if c > cost[v]:
             continue
-        for d, u in e[v]:
+        for d, u in edge[v]:
             tmp = d + cost[v]
             if tmp < cost[u]:
                 cost[u] = tmp
@@ -19,12 +18,12 @@ def dijkstra(s):
     return cost
 
 n,m = map(int,input().split())
-e = [[] for _ in range(n)]
+edge = [[] for _ in range(n)]
 for _ in range(m):
     a,b,t = map(int,input().split())
     a,b = a-1, b-1
-    e[a].append((t, b))
-    e[b].append((t, a)) # 有向グラフでは削除
+    edge[a].append((t, b))
+    edge[b].append((t, a)) # 有向グラフでは削除
 
 d = dijkstra(0)
 print([dijkstra(i) for i in range(n)])
