479f51b23213cf713212643639306ada72ed8902
so4649 <70653894+so4649@users.noreply.github.com>
2021-01-31 01:07:14 +0900

    Add files via upload

--- salesman.py
+++ salesman.py
@@ -1,5 +1,57 @@
-# https://kakedashi-engineer.appspot.com/2020/05/21/dpl2a/
+# https://qiita.com/merry1221/items/23a4204aa837122780d0
+# 非再帰
+v, e = map(int, input().split())
 
+INF = 10**10
+cost = [[INF]*v for _ in range(v)]
+
+for i in range(e):
+    s, t, d = map(int, input().split())
+    cost[s][t] = d
+
+#Dpは全体集合の部分集合Sについて最後がvであるという制約の下で順序を最適化したときのSの中での最小コスト
+dp = [[INF]*v for _ in range(1<<v)]
+dp[0][0] = 0
+
+#集合（訪れたか訪れていないかを表す二進数）
+for x in range(1<<v):
+    #最後に訪れたノード
+    for y in range(v):
+        #最後に訪れた以外のノード
+        for z in range(v):
+            #1.すでに訪れたかどうか 2.最後に訪れたノードではないか 3. yとzはそもそもつながっているのか
+            if x & (1<<y) and y != z and cost[z][y] < 10**6:
+                dp[x][y] = min(dp[x][y], dp[x^(1<<y)][z]+cost[z][y])
+
+if dp[-1][0] > 10**6:
+    print(-1)
+else:
+    print(dp[-1][0])
+
+
+# 0スタート時にまた0に戻ってくる解の関数化
+# 無理な場合INFで返る
+def salesman():
+    #Dpは全体集合の部分集合Sについて最後がvであるという制約の下で順序を最適化したときのSの中での最小コスト
+    dp = [[INF]*k for _ in range(1<<k)]
+    dp[0][0] = 0
+
+    #集合（訪れたか訪れていないかを表す二進数）
+    for x in range(1<<k):
+        #最後に訪れたノード
+        for y in range(k):
+            #最後に訪れた以外のノード
+            for z in range(k):
+                #1.すでに訪れたかどうか 2.最後に訪れたノードではないか 3. yとzはそもそもつながっているのか
+                if x & (1<<y) and y != z and cost[z][y] < 10**6:
+                    dp[x][y] = min(dp[x][y], dp[x^(1<<y)][z]+cost[z][y])
+
+    return dp[-1][0]
+
+
+
+
+# https://kakedashi-engineer.appspot.com/2020/05/21/dpl2a/
 V, E = map(int, input().split())
 INF = 10**10
 cost = [[INF]*V for _ in range(V)] # 重み
@@ -27,52 +79,3 @@ if ans == INF:
     print(-1)
 else:
     print (ans)
-
-
-# https://qiita.com/merry1221/items/23a4204aa837122780d0
-# 非再帰
-v, e = map(int, input().split())
-
-inf = 10**10
-cost = [[inf]*v for _ in range(v)]
-
-for i in range(e):
-    s, t, d = map(int, input().split())
-    cost[s][t] = d
-
-#Dpは全体集合の部分集合Sについて最後がvであるという制約の下で順序を最適化したときのSの中での最小コスト
-dp = [[inf]*v for _ in range(1<<v)]
-dp[0][0] = 0
-
-#集合（訪れたか訪れていないかを表す二進数）
-for x in range(1<<v):
-    #最後に訪れたノード
-    for y in range(v):
-        #最後に訪れた以外のノード
-        for z in range(v):
-            #1.すでに訪れたかどうか 2.最後に訪れたノードではないか 3. yとzはそもそもつながっているのか
-            if x & (1<<y) and y != z and cost[z][y] < 10**6:
-                dp[x][y] = min(dp[x][y], dp[x^(1<<y)][z]+cost[z][y])
-
-if dp[-1][0] > 10**6:
-    print(-1)
-else:
-    print(dp[-1][0])
-
-
-def salesman():
-    #Dpは全体集合の部分集合Sについて最後がvであるという制約の下で順序を最適化したときのSの中での最小コスト
-    dp = [[inf]*k for _ in range(1<<k)]
-    dp[0][0] = 0
-
-    #集合（訪れたか訪れていないかを表す二進数）
-    for x in range(1<<k):
-        #最後に訪れたノード
-        for y in range(k):
-            #最後に訪れた以外のノード
-            for z in range(k):
-                #1.すでに訪れたかどうか 2.最後に訪れたノードではないか 3. yとzはそもそもつながっているのか
-                if x & (1<<y) and y != z and cost[z][y] < 10**6:
-                    dp[x][y] = min(dp[x][y], dp[x^(1<<y)][z]+cost[z][y])
-
-    return dp[-1][0]
