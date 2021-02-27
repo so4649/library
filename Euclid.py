1d2f87ed6f81d7ab693977bd8e320c1fee084e23
so4649 <70653894+so4649@users.noreply.github.com>
2021-02-28 03:10:52 +0900

    Add files via upload

--- Euclid.py
+++ Euclid.py
@@ -47,3 +47,34 @@ def gcd_ext(a, b):
         a, b = b, a % b
         x, y, lastx, lasty = lastx - q * x, lasty - q * y, x, y
     return (lastx, lasty)
+
+
+
+# 中国剰余定理
+def extgcd(a, b):
+    if b:
+        d, y, x = extgcd(b, a % b)
+        y -= (a // b) * x
+        return d, x, y
+    return a, 1, 0
+
+# V = [(X_i, Y_i), ...]: X_i (mod Y_i)
+# xが答え,dはmodのlcm(最小公倍数)
+def remainder(V):
+    x = 0; d = 1
+    for X, Y in V:
+        g, a, b = extgcd(d, Y)
+        x, d = (Y*b*x + d*a*X) // g, d*(Y // g)
+        x %= d
+        
+    # 条件をみたす答えが存在しなくても出してしまうので確かめる必要がある
+    for X, Y in V:
+        if x % Y != X:
+            return -1
+
+    return x, d
+
+v = ((5,12),(9,16))
+print(remainder(v))
+v = ((3,14),(5,14))
+print(remainder(v))
