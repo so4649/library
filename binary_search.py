4142a9bea0ec104ac8f8d5828d15b19eb1b1ca60
so4649 <70653894+so4649@users.noreply.github.com>
2021-03-03 20:42:08 +0900

    Add files via upload

--- binary_search.py
+++ binary_search.py
@@ -1,20 +1,3 @@
-def binary_search(data, value):
-    left = 0            # 探索する範囲の左端を設定
-    right = len(data) - 1            # 探索する範囲の右端を設定
-    while left <= right:
-        mid = (left + right) // 2            # 探索する範囲の中央を計算
-        if data[mid] == value:
-            # 中央の値と一致した場合は位置を返す
-            return mid
-        elif data[mid] < value:
-            # 中央の値より大きい場合は探索範囲の左を変える
-            left = mid + 1
-        else:
-            # 中央の値より小さい場合は探索範囲の右を変える
-            right = mid - 1
-    return -1            # 見つからなかった場合
-
-
 # https://qiita.com/ta7uw/items/d6d8f0ddb215c3677cd3
 # 二分探索木
 import bisect
@@ -40,13 +23,11 @@ index = bisect.bisect_right(A, 3) # 5
 # 参考2：https://qiita.com/drken/items/97e37dd6143e33a64c8c
 # 範囲を上に開な半開区間で持つことで+1,-1が不要になり余計なバグを生みづらくなる
 # また、範囲を[l, r)でなく[ok, ng)あるいは[ng, ok)のように定義することで
-# 上限・下限のいずれを求めたいときも、abs(ok - ng) > 1という同じループ条件で
-# 動作させることができる
-# (上のライブラリも半開に書き換えた方がいいかも？)
+# 上限・下限のいずれを求めたいときも、abs(ok - ng) > 1という同じループ条件で動作させることができる
 def binary_search_func(ok, ng, f):
     while(abs(ok - ng) > 1):
         med = (ok + ng) // 2
-        if(f(med) == True):
+        if f(med) == True:
             ok = med
         else:
             ng = med
@@ -55,11 +36,30 @@ def binary_search_func(ok, ng, f):
 
 
 # floatの時の,条件Cを満たす二分探索（蟻本）
-lb, ub = 0, 10**6
-for i in range(100): # while ub - lb > 1:
-    mid = (lb + ub) / 2
-    if C(mid):
-        lb = mid
+ok, ng = 0, 10**6
+for i in range(100): # while (ng - ok) > 1:
+    mid = (ok + ng) / 2
+    if f(mid):
+        ok = mid
     else:
-        ub = mid
-print(lb)
+        ng = mid
+print(ok)
+
+
+
+# 初めの頃使ってた値探索（しかし、setのinで十分なので不要か？）
+def binary_search(data, value):
+    left = 0            # 探索する範囲の左端を設定
+    right = len(data) - 1            # 探索する範囲の右端を設定
+    while left <= right:
+        mid = (left + right) // 2            # 探索する範囲の中央を計算
+        if data[mid] == value:
+            # 中央の値と一致した場合は位置を返す
+            return mid
+        elif data[mid] < value:
+            # 中央の値より大きい場合は探索範囲の左を変える
+            left = mid + 1
+        else:
+            # 中央の値より小さい場合は探索範囲の右を変える
+            right = mid - 1
+    return -1            # 見つからなかった場合
