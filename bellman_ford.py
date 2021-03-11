a2b3b3717603be33b8b385ef9d95d6f73bb856e4
so4649 <70653894+so4649@users.noreply.github.com>
2021-03-12 07:22:00 +0900

    Add files via upload

--- bellman_ford.py
+++ bellman_ford.py
@@ -1,9 +1,13 @@
 # O(EV)
+# 負閉路を含むパスがある終点は-INFにする
+INF = 10**15
 def bellman_ford(s):
-    d = [float('inf')]*n # 各頂点への最小コスト
-    d[s] = 0 # 自身への距離は0
+    d = [INF]*n # 各頂点への最小コスト
+    d[s] = 0
+
     for i in range(n):
         update = False # 更新が行われたか
+        ok = True
         for x, y, z in g:
             if d[y] > d[x] + z:
                 d[y] = d[x] + z
@@ -12,15 +16,21 @@ def bellman_ford(s):
             break
         # 負閉路が存在
         if i == n - 1:
-            return -1
+            ok = False
+    if ok:
+        return d
+    
+    # 負閉路を含むパスがある終点の値を-INFにする
+    for i in range(n):
+        for x, y, z in g:
+            if d[y] > d[x] + z:
+                d[y] = -INF
     return d
 
-n, w = [int(x) for x in input().split()] # n:頂点数, w:辺の数
+n,m = [int(x) for x in input().split()]
 g = []
-for _ in range(w):
+for _ in range(m):
     x, y, z = [int(x) for x in input().split()] # 始点,終点,コスト
     g.append([x, y, z])
     g.append([y, x, z]) # 有向グラフでは削除
 print(bellman_ford(0))
-
-# https://qiita.com/takayg1/items/92fb138683ac72b47d86
