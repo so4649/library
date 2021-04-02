80751391c3243a3b442af3863b68f98d110bbf0e
so4649 <70653894+so4649@users.noreply.github.com>
2021-04-03 07:42:07 +0900

    Add files via upload

--- Unionfind.py
+++ Unionfind.py
@@ -50,30 +50,6 @@ class UnionFind():
 
 #URL
 #https://note.nkmk.me/python-union-find/
-# parents
-# 各要素の親要素の番号を格納するリスト
-# 要素が根（ルート）の場合は-(そのグループの要素数)を格納する
-# find(x)
-# 要素xが属するグループの根を返す
-# union(x, y)
-# 要素xが属するグループと要素yが属するグループとを併合する
-# size(x)
-# 要素xが属するグループのサイズ（要素数）を返す
-# same(x, y)
-# 要素x, yが同じグループに属するかどうかを返す
-# members(x)
-# 要素xが属するグループに属する要素をリストで返す
-# 関連記事: Pythonリスト内包表記の使い方
-# roots()
-# すべての根の要素をリストで返す
-# group_count()
-# グループの数を返す
-# all_group_members
-# {ルート要素: [そのグループに含まれる要素のリスト], ...}の辞書を返す
-# 関連記事: Pythonで辞書を作成するdict()と波括弧、辞書内包表記
-# __str__()
-# print()での表示用
-# ルート要素: [そのグループに含まれる要素のリスト]を文字列で返す
 
 uf = UnionFind(6)
 print(uf.parents)
@@ -109,3 +85,112 @@ print(uf.size(0))
 
 print(uf.all_group_members())
 # {0: [0, 2], 1: [1], 3: [3], 4: [4], 5: [5]}
+
+
+
+
+
+# 部分永続Union-Find (partially persistent union-find)
+# O(logN)
+
+# N: 頂点数
+# p[u]: 頂点uの親頂点
+# sz[u]: 現在の親頂点uの木が含む頂点数
+# H[u]: 現在の親頂点uの木の高さ
+# S[u] = [(t, s), ...]:
+#     時刻tに別の頂点をマージした親頂点uの木が含む頂点数s
+# T[u]: 頂点uが親頂点でなくなる時刻
+
+from bisect import bisect
+INF = 1e18
+class PP_UnionFind():
+    def __init__(self, N):
+        self.N = N
+        *self.p, = range(self.N)
+        self.sz = [1]*self.N
+        self.H = [1]*self.N
+        self.S = [[(0, 1)] for i in range(self.N)]
+        self.T = [INF]*self.N
+
+    def find(self, x, t):
+        while self.T[x] <= t:
+            x = self.p[x]
+        return x
+
+    def union(self, x, y, t):
+        px = self.find(x, t)
+        py = self.find(y, t)
+        if px == py:
+            return 0
+        if self.H[py] < self.H[px]:
+            self.p[py] = px
+            self.T[py] = t
+            self.sz[px] += self.sz[py]
+            self.S[px].append((t, self.sz[px]))
+        else:
+            self.p[px] = py
+            self.T[px] = t
+            self.sz[py] += self.sz[px]
+            self.S[py].append((t, self.sz[py]))
+            self.H[py] = max(self.H[py], self.H[px]+1)
+        return 1
+
+    def size(self, x, t):
+        y = self.find(x, t)
+        idx = bisect(self.S[y], (t, INF))-1
+        return self.S[y][idx]
+
+
+
+
+
+# 重み付きUnionfind
+# O(logN)
+
+# union(x, y, w):weight(y) = weight(x) + w となるように x と y をマージする
+# same(x, y):x と y が同じグループにいるかどうかを判定する
+# dist(x, y):x と y とが同じグループにいるとき、weight(y) - weight(x) をリターンする
+
+class W_UnionFind:
+    def __init__(self, n):
+        self.p=[i for i in range(n)]
+        self.r=[0 for i in range(n)]
+        self.d=[0 for i in range(n)]
+
+    def root(self,x):
+        if self.p[x]==x:
+            return x
+        else:
+            tmp = self.root(self.p[x])
+            self.d[x] += self.d[self.p[x]]
+            self.p[x] = tmp
+            return self.p[x]
+    
+    def weight(self,x):
+        self.root(x)
+        return self.d[x]
+
+    def dist(self,x,y):
+        if self.root(x)==self.root(y):
+            return self.weight(y)-self.weight(x)
+        else:
+            return '?'
+
+    def union(self,x,y,w):
+        w+=self.weight(x)
+        w-=self.weight(y)
+        x=self.root(x)
+        y=self.root(y)
+        if(x==y):
+            return
+        if self.r[x]<self.r[y]:
+            self.p[x]=y
+            self.d[x]=-w
+        else:
+            self.p[y]=x
+            self.d[y]=w
+            if self.r[x]==self.r[y]:
+                self.r[x]+=1
+    
+    def same(self,x,y):
+        return self.root(x)==self.root(y)
