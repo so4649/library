073b8a99193904518703927ab606863700f042d1
so4649 <70653894+so4649@users.noreply.github.com>
2021-04-12 15:58:59 +0900

    Add files via upload

--- imos.py
+++ imos.py
@@ -17,9 +17,18 @@ class Imos:
     def get(self,i):
         return sum(self.im[:i+1])
 
+    # 最終的な配列を返す。O(n)
+    def get_array(self):
+        ruiseki = [0]*self.N
+        ruiseki[0] = self.im[0]
+        for i in range(1,self.N):
+            ruiseki[i] = ruiseki[i-1]+self.im[i]
+        return ruiseki
+
 a = [1,3,5,2,3]
 imos = Imos(a)
 # a[2]~a[4]に2足す
 imos.add(2,5,2)
 print(imos.get(1)) # 3
 print(imos.get(4)) # 5
+print(imos.get_array()) # [1, 3, 7, 4, 5]
