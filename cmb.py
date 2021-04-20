6da02401436bf9972a48c469252a8c8f5a46d026
so4649 <70653894+so4649@users.noreply.github.com>
2021-04-20 15:13:12 +0900

    Add files via upload

--- cmb.py
+++ cmb.py
@@ -56,6 +56,47 @@ def c(n, r):
         res = res * (n - i) // (i + 1)
     return res
 
+
+
+# lucasの定理を用いたnCk(modp)の求め方
+# mod3などの時はこれを用いる必要がある
+
+def c(n, r):
+    if n < 0 or r < 0 or n < r:
+        return 0
+    r = min(r, n - r)
+    res = 1
+    for i in range(r):
+        res = res * (n - i) // (i + 1)
+    return res
+
+# xをp進法表記のリストに変換
+def p_ary(x, p):
+    res = []
+    while x:
+        res.append(x%p)
+        x //= p
+    return res[::-1]
+
+# C(n,r)をpで割った余りを返す
+def lucas(n, r, p):
+    # nとrをp進法で表記
+    n_p = p_ary(n, p)
+    r_p = p_ary(r, p)
+    # n_pとr_pの桁を揃える(左側を0で埋める)
+    r_p = [0]*(len(n_p)-len(r_p)) + r_p
+
+    res = 1
+    for i,j in zip(n_p, r_p):
+        res *= c(i,j)
+        res %= p
+    return res
+
+ans = lucas(15,1,14)
+print(ans)
+
+
+
 #itertools
 import itertools
 
